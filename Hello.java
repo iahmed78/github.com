7c3748b5a771006eff535452bf7d5e287ec978a8
iahmed78 <tiftiam@yahoo.com>
2013-07-28 18:01:58 -0700

    Create Hello.java
    Test to add new file

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+  public String HelloWorld {
+  
+    return "Hello New World order";
+  
+  }
+  
+  public static void main (String [] args) {
+  
+    System.out.println(HelloWorld());
+  
+  }
+
+}
